329a2c95226617d9c32d7bdb7e7f2e346b844865
liviaaoliveiraa <lligiaoliveira00@gmail.com>
2025-09-15 14:40:09 -0300

    cria tabela de artista

---
+++ queries/01-insert-artists.sql
@@ -0,0 +1,11 @@
+CREATE TABLE artistas (
+    id SERIAL PRIMARY KEY NOT NULL,
+    nome VARCHAR(100) NOT NULL,
+    tipo VARCHAR(20) NOT NULL,
+    membros INTEGER,
+    genero_musical VARCHAR(30),
+    ano_lancamento INTEGER,
+    pais_origem VARCHAR(50),
+    musica_famosa VARCHAR(50),
+    ativo BOOLEAN
+);
