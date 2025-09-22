cd7ec9ca20d8c7c625f1f99a96dd361b0beb3156
liviaaoliveiraa <lligiaoliveira00@gmail.com>
2025-09-22 11:22:27 -0300

    Realizando o Delete e Update

--- atividade-02/03-update.sql
+++ atividade-02/03-update.sql
@@ -1,1 +1,8 @@
 UPDATE proditos SET preco = preco * 0.8;
+
+UPDATE produtos SET estoque = estoque = 50;
+
+UPDATE produtos SET preco = preco * 1.58;
+
+UPDATE produtos SET preco *preco * 0.9
+WHERE estoque < 100;
---
+++ atividade-02/04- delete.sql
@@ -0,0 +1,8 @@
+DELETE  from produtos
+WHERE estoque < 10;
+
+DELETE from produtos
+WHERE  preco < 20.00;
+
+DELETE FROM produtos
+WHERE marca =  'Maybelline';
